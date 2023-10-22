5666363e2d28e6da091734c70a9fe393d25712f1
JuHyeonPark <parkjh080@naver.com>
2023-10-22 23:10:14 +0900

    feat: 초기 세팅 값
    초기 세팅 값 설정

---
+++ src/main/java/com/example/ToyProject2/ToyProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.ToyProject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ToyProject2/ToyProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToyProject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
